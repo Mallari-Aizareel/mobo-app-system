23e1ec3fde9734fc59fad008ca88aac05d11d446
Mallari-Aizareel <aizareelmallari000@gmail.com>
2025-09-10 11:16:31 +0800

    Camera Fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Permission.camera.request();
+
   runApp(const MyApp());
 }
 
@@ -14,24 +19,58 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: InAppWebView(
-            initialUrlRequest: URLRequest(url: WebUri("http://192.168.1.19/mobo/public/portal/login")),
-            initialOptions: InAppWebViewGroupOptions(
-              android: AndroidInAppWebViewOptions(useHybridComposition: true),
-              crossPlatform: InAppWebViewOptions(javaScriptEnabled: true),
-            ),
-            onWebViewCreated: (controller) {
-              print("WebView Created");
-            },
-            onLoadError: (controller, url, code, message) {
-              print("Load error: $message");
-            },
-            onConsoleMessage: (controller, consoleMessage) {
-              print(consoleMessage);
-            },
-          ),
+          child: WebViewContainer(),
         ),
       ),
+    );
+  }
+}
+
+class WebViewContainer extends StatefulWidget {
+  const WebViewContainer({Key? key}) : super(key: key);
+
+  @override
+  State<WebViewContainer> createState() => _WebViewContainerState();
+}
+
+class _WebViewContainerState extends State<WebViewContainer> {
+  late InAppWebViewController webViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return InAppWebView(
+      initialUrlRequest: URLRequest(
+        url: WebUri("http://192.168.1.19/mobo/public/portal/login"),
+      ),
+      initialOptions: InAppWebViewGroupOptions(
+        android: AndroidInAppWebViewOptions(
+          useHybridComposition: true,
+          mediaPlaybackRequiresUserGesture: false,
+        ),
+        crossPlatform: InAppWebViewOptions(
+          javaScriptEnabled: true,
+        ),
+      ),
+      onWebViewCreated: (controller) {
+        webViewController = controller;
+        print("WebView Created");
+      },
+
+      onPermissionRequest: (controller, request) async {
+        print("Permission requested: ${request.resources}");
+        return PermissionRequestResponse(
+          resources: request.resources,
+          action: PermissionRequestResponseAction.GRANT,
+        );
+      },
+
+      onLoadError: (controller, url, code, message) {
+        print("Load error: $message");
+      },
+
+      onConsoleMessage: (controller, consoleMessage) {
+        print("Console: ${consoleMessage.message}");
+      },
     );
   }
 }
